838514b4bd8973af55abfdb0149f9215ae772b33
Scott Matsuda <smatsuda2014@gmail.com>
2019-09-30 20:09:50 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 # Add your variables here
 
-int first_number = 1
-int second_number = 2
+first_number = 1
+second_number = 2
 
 var sum = first_number + second_number
 var subtraction = first_number - second_number
