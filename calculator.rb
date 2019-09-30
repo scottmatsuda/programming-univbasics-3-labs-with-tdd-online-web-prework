d042bdd0841095085b054909bd6f3457debcb50f
Scott Matsuda <smatsuda2014@gmail.com>
2019-09-30 20:10:56 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,6 +4,6 @@ first_number = 1
 second_number = 2
 
 sum = first_number + second_number
-subtraction = first_number - second_number
+difference = first_number - second_number
 product = first_number * second_number
 quotient = first_number / second_number
