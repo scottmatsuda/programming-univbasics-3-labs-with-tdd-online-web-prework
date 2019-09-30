06552acb7aee766ee462f22573c30abf5c925dbb
Scott Matsuda <smatsuda2014@gmail.com>
2019-09-30 20:07:45 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,3 +2,8 @@
 
 first_number = 1
 second_number = 2
+
+sum = first_number + second_number
+subtraction = first_number - second_number
+product = first_number * second_number
+division = first_number / second_number
